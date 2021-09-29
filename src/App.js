4cd2395d005c8de1e5eb08adeb9f0329e411dc64
Naveen Busireddy <84928714+naveenbusireddy@users.noreply.github.com>
2021-09-29 12:28:27 +0530

    Update: Style

--- src/App.js
+++ src/App.js
@@ -20,7 +20,8 @@ var emojisWeKnow = Object.keys(emojiDictionary);
 export default function App() {
   const [meaning, setMeaning] = useState("");
 
-  function emojiInputHandler(event) { //processing
+  function emojiInputHandler(event) {
+    //processing
     var userInput = event.target.value;
     var meaning = emojiDictionary[userInput];
     if (meaning === undefined) {
@@ -29,7 +30,8 @@ export default function App() {
     setMeaning(meaning); //react call to show output
   }
 
-  function emojiClickHandler(emoji) { //processing
+  function emojiClickHandler(emoji) {
+    //processing
     var meaning = emojiDictionary[emoji];
     setMeaning(meaning); //React call to show output
   }
@@ -37,18 +39,23 @@ export default function App() {
     <div className="App">
       <h1>click on the emoji to find their meaning</h1>
       <input onChange={emojiInputHandler}></input>
-      <h2>{meaning}</h2> 
+      <h2>{meaning}</h2>
       {/* Actual output set by React using useState */}
 
       <h3>emojis we know</h3>
-      
-      { emojisWeKnow.map(function(emoji){
-        return <span onClick={()=>emojiClickHandler(emoji)} style={{fontSize:"1.5rem", padding:"0.1rem", cursor:"pointer"}} key={emoji}> {emoji} </span>
-        })
-      }
 
+      {emojisWeKnow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "1.5rem", padding: "0.1rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {" "}
+            {emoji}{" "}
+          </span>
+        );
+      })}
     </div>
-
-    
   );
 }
